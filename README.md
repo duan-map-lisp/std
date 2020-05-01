d55248cb2174c8feae59553c2304b150047eb07f
duan <naonao800@163.com>
2020-05-01 10:20:29 +0800

    添加了个数字加法测试一下，暂时能用。

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# duan_map_lisp
+# github.com/duan-map-lisp/std
 
 lisp是list模型，也就是S表达式列表式模型，忽然突发奇想到如果是用Map模型替代lisp的list模型，仿lisp是不是能创造出一个更好的语言？于是，有了这个项目。
 
 先写个编译器，命名为dl。
+
+这个是标准库。
---
+++ math/math.go
@@ -0,0 +1,48 @@
+package math
+
+import (
+	"fmt"
+
+	"github.com/duan-map-lisp/dl"
+)
+
+func Add (self *dl.Dl) (res interface{}) {
+	var err error
+	var name string
+	if len (self.SubNodeList) <= 0 {
+		panic ("subNodeList len is 0")
+	}
+
+	if len (self.SubNodeList) == 1 {
+		res = 0
+		return
+	}
+
+	if name, err = self.SubNodeListGetString (0); err != nil {
+		panic (err)
+	}
+	if name != "+" {
+		panic ("函数名异常:" + name)
+	}
+
+	tmpRes := float64 (0)
+	for _, One := range self.SubNodeList {
+		resI := One.Run ()
+
+		switch resTmp := resI.(type) {
+		case float64:
+			tmpRes += resTmp
+		case string:
+			if resTmp != "+" {
+				fmt.Println ("参数异常")
+				panic (resTmp)
+			}
+		default:
+			fmt.Println ("参数异常")
+			panic (resTmp)
+		}
+	}
+	res = tmpRes
+
+	return
+}
